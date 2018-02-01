6cb64cc341e8af9061ea0ed436a6169d2eb19c90
aemann <allison.e.mann@gmail.com>
2018-01-31 20:06:59 -0600

    final clean up

--- chop.py
+++ chop.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-'''Useage: python chop.py -f input.fasta -r <number of reads to generate> -i <mean insert length> -sd <standard deviation of insert sizes> -t <sequencing type (paired or single)> -l <maximum sequencing length>'''
+'''Useage: python chop.py -f input.fasta -r <number of reads to generate> -i <mean insert length> -t <sequencing type (paired or single)> [optional: -sd <standard deviation of insert size normal distribution> -l <maximum sequencing length> -a <adapter sequence>]'''
 
 import sys
 import numpy as np
@@ -11,23 +11,23 @@ from Bio import SeqIO
 
 #set up script parameters
 parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--fasta', help='input fasta file')
-parser.add_argument('-r', '--reads', help='number of reads to simulate', default='1')
-parser.add_argument('-sd', '--stddev', help='standard deviation of fragment length distribution', default='10')
-parser.add_argument('-i', '--insertSize', help='mean fragment length (insert size)', default='70')
+parser.add_argument('-f', '--fasta', help='input fasta file', required=True)
+parser.add_argument('-r', '--reads', help='number of reads to simulate', required=True)
+parser.add_argument('-sd', '--stddev', help='standard deviation of fragment length distribution', default='50')
+parser.add_argument('-i', '--insertSize', help='mean fragment length (insert size)', required=True)
 parser.add_argument('-l', '--maxlen', help='maximum length of read based on sequencing chemistry', default='200')
-parser.add_argument('-t', '--seqtype', help='paired or single', default='single')
+parser.add_argument('-t', '--seqtype', help='paired or single', required=True)
 parser.add_argument('-a', '--adapter', help='adapter sequence', default='xxxxxxxxxx')
 
 args = parser.parse_args()
+
+#check if file exists  
+assert os.path.exists(args.fasta), bcolors.WARNING + 'Yeah, either %s does not exist or you need to include the path' % args.fasta
 
 #color text
 class bcolors:
 	WARNING = '\033[91m'
 	COMPLETE = '\033[92m'
-
-#check if file exists, parameters set
-assert os.path.exists(args.fasta), bcolors.WARNING + 'Yeah, either %s does not exist or you need to include the path' % args.fasta
 
 #dictionary of fasta headers that are in the file (in case of multi fasta input)
 names = [rec.name for rec in SeqIO.parse(args.fasta, "fasta")]
@@ -76,4 +76,6 @@ if args.seqtype == 'paired':
         pairedEnd(fastaDict, adapter)
 elif args.seqtype == 'single':
         singleEnd(fastaDict, adapter)
+else:
+	print("SeqType must be either paired or single")
 
